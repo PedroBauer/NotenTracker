4785182b09bfa6db4f73fab56a4ba44b60936eb3
PedroBauer <pgm.bauer@gmail.com>
2014-06-17 10:58:18 +0200

    0

--- NotenTracker/gen/com/PBCDKH/notentracker/R.java
+++ NotenTracker/gen/com/PBCDKH/notentracker/R.java
@@ -14,11 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f060008;
         public static final int container=0x7f060000;
+        public static final int editText1=0x7f060006;
+        public static final int editText2=0x7f060003;
         public static final int tableRow1=0x7f060001;
-        public static final int tableRow3=0x7f060003;
-        public static final int tableRow4=0x7f060004;
+        public static final int tableRow3=0x7f060004;
+        public static final int tableRow4=0x7f060007;
         public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060005;
     }
     public static final class layout {
         public static final int login_main=0x7f030000;
